9d795256bc4bee5d9b156c5333cf3224bf38728f
John Traut <johntraut@Johns-MacBook-Pro.local>
2022-01-23 16:14:35 -0800

    first hosting push

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a4510075b45bc88416630679f2e1bc81'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2c44e80d-b1ea-4bd3-9982-bddc02e9f5fc'
+};
